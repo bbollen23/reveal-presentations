27d9754bc74311dc0ca4ad2d189602fc04311b3d
BBollen-VivoBook <bbollen23@gmail.com>
2021-03-16 20:33:01 -0700

    new commit

--- js/other.js
+++ js/other.js
@@ -1,6 +1,6 @@
 let div = d3.selectAll(".smoothing").select(".bar").call(d3.drag()
   .on("drag",function(event,d){
-    let value = event.y < -15 ? -365 : (event.y > 350) ? 0 : event.y-350;
+    let value = event.y < 15 ? -435 : (event.y > 550) ? 100 : event.y-450;
     d3.select(this).attr("y",value);
 }));    
 
